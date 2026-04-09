2a867c514466f7653b375ac723419f68a240e4ca
SangBelalang-ops <sangbelalangsembah@gmail.com>
2026-04-09 12:33:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,10 @@
-async function cekRekening() {
-    const bank = document.getElementById('bankCode').value;
-    const norek = document.getElementById('accNumber').value;
-    const hasil = document.getElementById('result');
-    
-    const API_KEY = "aFVBKbUGW15JPwN3to5L7DiyN0QY1syLI4p4jVQ5N0VnsgYHRs"; 
-    const BASE_URL = "https://use.api.co.id/validation/bank";
+// Tes pake URL paling dasar sesuai dokumentasi terbaru mereka
+const testUrl = `https://use.api.co.id/v1/bank/inquiry?bank_code=${bank}&account_number=${norek}`;
 
-    if(!bank || !norek) return alert("Pilih bank dan isi nomor rekening!");
-
-    hasil.innerHTML = "<span style='color:cyan'>Lagi diintip...</span>";
-
-    // TRIK: Kita kasih nama asal "Cek" biar server mau proses
-    const params = new URLSearchParams({
-        bank_code: bank,
-        account_number: norek,
-        account_name: "Cek" 
-    });
-
-    try {
-        const response = await fetch(`${BASE_URL}?${params.toString()}`, {
-            method: 'GET',
-            headers: { 'x-api-co-id': API_KEY }
-        });
-
-        const res = await response.json();
-
-        // Di dokumentasi lo, kalau rek ketemu, dia tetep balikin field 'name' 
-        // meskipun is_valid mungkin false (karena nama "Cek" gak cocok sama nama asli)
-        if (res.name) {
-            hasil.innerHTML = `NAMA PEMILIK:<br><b style="color:#00ff00; font-size:22px;">${res.name.toUpperCase()}</b>`;
-        } else {
-            hasil.innerHTML = `<span style='color:#ffae00;'>${res.message || "Gak Ketemu, Bro!"}</span>`;
-        }
-    } catch (err) {
-        hasil.innerHTML = "<span style='color:red'>Server API Ngambek!</span>";
+const response = await fetch(testUrl, {
+    method: 'GET',
+    headers: {
+        'x-api-co-id': 'aFVBKbUGW15JPwN3to5L7DiyN0QY1syLI4p4jVQ5N0VnsgYHRs',
+        'Content-Type': 'application/json'
     }
-}
+});
